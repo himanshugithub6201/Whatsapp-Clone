e6c86493c909c8d4b7b6eee51e9f21625b8e3951
HimanshuPandey <himanshugithub6201@gmail.com>
2020-08-30 09:19:47 +0530

    1st commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBbCQ3wbGsLFOkWYvXa76r5xiEWqxDY7wM",
+  authDomain: "whatsapp-clone-86870.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-86870.firebaseio.com",
+  projectId: "whatsapp-clone-86870",
+  storageBucket: "whatsapp-clone-86870.appspot.com",
+  messagingSenderId: "576608132415",
+  appId: "1:576608132415:web:0e12ff5380bc5ece39c78a",
+};
